28568a17dd33aaf7d69d61d678364d1009177496
jethrowang <jethrowang0531@gmail.com>
2025-12-01 16:26:52 +0800

    inference

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from omnilingual_asr.models.inference.pipeline import ASRInferencePipeline
 
-pipeline = ASRInferencePipeline(model_card="omniASR_LLM_7B")
+pipeline = ASRInferencePipeline(model_card="omniASR_LLM_1B")
 
 audio_files = ["/path/to/eng_audio1.flac", "/path/to/deu_audio2.wav"]
 lang = ["eng_Latn", "deu_Latn"]
--- inference_hf.py
+++ inference_hf.py
@@ -4,14 +4,15 @@ from omnilingual_asr.models.inference.pipeline import ASRInferencePipeline
 # Load dataset for a specific language (e.g., Ligurian)
 omni_dataset = load_dataset("facebook/omnilingual-asr-corpus", "lij_Latn", split="train", streaming=True)
 batch = next(omni_dataset.iter(5))
-print(f'Batch: {batch}')
+print(f'Batch:\n{batch}')
 
 # Convert to pipeline input format
 audio_data = [{"waveform": x["array"], "sample_rate": x["sampling_rate"]}
               for x in batch["audio"]]
+print(f'Audio Data:\n{audio_data}')
 
 # Run inference
-pipeline = ASRInferencePipeline(model_card="omniASR_LLM_7B")
+pipeline = ASRInferencePipeline(model_card="omniASR_LLM_1B")
 transcriptions = pipeline.transcribe(audio_data, batch_size=2)
 
 # Display results
