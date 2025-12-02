617e1ed92a0dc3729dd5dce322a9eaf741179dd0
jethrowang <jethrowang0531@gmail.com>
2025-12-02 11:09:24 +0800

    inference

---
+++ inference_minds14.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+from omnilingual_asr.models.inference.pipeline import ASRInferencePipeline
+
+ds = load_dataset("PolyAI/minds14", "zh-CN", split="train", streaming=True)
+# print(next(iter(ds)))
+batch = next(ds.iter(5))
+print(f'Batch:\n{batch}')
+
+# Convert to pipeline input format
+audio_data = [{"waveform": x["array"], "sample_rate": x["sampling_rate"]}
+              for x in batch["audio"]]
+print(f'Audio Data:\n{audio_data}')
+
+# Run inference
+pipeline = ASRInferencePipeline(model_card="omniASR_LLM_7B")
+transcriptions = pipeline.transcribe(audio_data, batch_size=2)
+
+# Display results
+for i, (transcription, original_text) in enumerate(zip(transcriptions, batch["transcription"]), 1):
+    print(f"\n Sample {i}:")
+    print(f"   Ground Truth: {original_text}")
+    print(f"   Predicted:    {transcription}")
